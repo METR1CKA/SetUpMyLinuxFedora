c2dc894310bf42eb31c9e3eefca3ece7e7874172
METR1CKA <ferchosalazar054@gmail.com>
2023-07-11 16:12:13 -0500

    [UPD] - Version 1.5
    - Format file script

--- setup.sh
+++ setup.sh
@@ -12,76 +12,57 @@ grayColour="\e[0;37m\033[1m"
 
 trap ctrl_c INT
 
+DNF_CONF="./dnf.conf.txt"
+
 # Cancelar procesos
-function ctrl_c() {
+ctrl_c() {
   echo -e "\n${redColour}[!] Cancelando procesos...${endColour}"
   exit
 }
 
 # Tiempo de espera
-function slp() {
+slp() {
   sleep 2
 }
 
 # Modificar carga de repos
-function mod_repos() {
+mod_repos() {
   echo -e "\n${blueColour}[*] Añadiendo configuraciones a dnf${endColour}"
-
   slp
-
-  content=$(cat "./dnf.conf.txt")
-
+  content=$(cat "$DNF_CONF")
   echo "$content" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
-
   echo -e "\n${greenColour}[+] Contenido añadido al archivo${endColour}"
 }
 
 # Actualización de repos
-function update_upgrade() {
+update_upgrade() {
   echo -e "\n${blueColour}[*] Limpiando cache${endColour}"
-
   sudo dnf clean all
-
   echo -e "\n${blueColour}[*] Actualizando repositorios${endColour}"
-
   sudo dnf check-update
-
   sudo dnf update -y && sudo dnf upgrade -y
-
   echo -e "\n${greenColour}[+] Repositorios actualizados${endColour}"
 }
 
-function rpm_fusion() {
+rpm_fusion() {
   echo -e "\n${blueColour}[*] Habilitando RPM fusion${endColour}"
-
   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-
   sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
   sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-
   sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
   echo -e "\n${blueColour}[*] Actualizando grupo${endColour}"
-
   sudo dnf groupupdate core
-
   echo -e "\n${greenColour}[+] RPM Habilitado${endColour}"
 }
 
-function ch_hostname() {
+ch_hostname() {
   echo -e "\n${blueColour}[+] ¿Desea modificar el hostname?${endColour}\n"
-  
   read -p "(s/n): " option
-
   if [[ $option == "s" || $option == "S" ]]
   then
     echo -e "\n${greenColour}[+] Ingrese el nuevo nombre del host${endColour}\n"
-
     read -p "Nombre: " name
-
     sudo hostnamectl set-hostname $name
-
     echo -e "\n${greenColour}[+] Hostname modificado con exito${endColour}"
   elif [[ $option == "n" || $option == "N" ]]
   then
@@ -91,56 +72,11 @@ function ch_hostname() {
   fi
 }
 
-function codecs() {
+codecs() {
   echo -e "\n${blueColour}[*] Instalando codecs multimedia${endColour}"
-
   sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-
   sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-
   echo -e "\n${greenColour}[+] Codecs instalados con exito${endColour}"
-}
-
-function main() {
-  slp
-
-  echo -e "\n${yellowColour}[!] Iniciando procesos${endColour}"
-
-  slp
-
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-
-  mod_repos
-
-  slp
-
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-  
-  update_upgrade
-
-  slp
-
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-
-  rpm_fusion
-
-  slp
-  
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-
-  ch_hostname
-
-  slp
-
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-
-  codecs
-
-  slp
-
-  echo -e "\n${grayColour}----------------------------------------${endColour}"
-
-  echo -e "\n${greenColour}[+] Proceso terminado${endColour}"
 }
 
 # Permisos root
@@ -150,4 +86,26 @@ then
   exit
 fi
 
-main
+# Init process
+slp
+echo -e "\n${yellowColour}[!] Iniciando procesos${endColour}"
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+mod_repos
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+update_upgrade
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+rpm_fusion
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+ch_hostname
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+codecs
+slp
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+echo -e "\n${greenColour}[+] Proceso terminado${endColour}"
+echo -e "\n${grayColour}----------------------------------------${endColour}"
+exit 0
